de7485ddaa531ac1f049554243c76903c9344c65
Ben <berkhart@rocketmail.com>
2022-08-24 23:15:53 -0500

    Completed review

--- src/index.js
+++ src/index.js
@@ -1,65 +1,59 @@
-// write your code here
-// See all ramen images in the `div` with the id of `ramen-menu`. When the page
-//   loads, request the data from the server to get all the ramen objects. Then,
-//   display the image for each of the ramen using an `img` tag inside the
-//   `#ramen-menu` div.
-
-// Click on an image from the `#ramen-menu` div and see all the info about that
-// ramen displayed inside the `#ramen-detail` div and where it says
-// `insert comment here` and `insert rating here`.
-
-let ramens = [];
-
-const ramenMenu = document.querySelector('#ramen-menu');
-
-
 fetch('http://localhost:3000/ramens')
-.then((res) => res.json())
-.then((data) => {
-    console.log(data)
-    ramens = data
-    selectedRamenId = data[0].id
-    ramenArray()
-});
+.then ((res) => res.json())
+.then ((data) => data.forEach((ramen) => {
+    renderRamen(ramen)
+}));
 
-function ramenArray () {
-    ramens.forEach ((ramen) => {
-     renderMenu(ramen)
-     imageClick()
-    })
-}
-
-function renderMenu (ramen) {
-        const ramenImg = document.createElement('img')
-        ramenImg.src = ramen.image
-        ramenMenu.append(ramenImg);
-        ramenImg.dataset.id = ramen.id
-        console.log(ramen);
-    }
-
-
-
-function imageClick () {
-    const detailImage = document.querySelector('.detail-image')
-    const ramenDetail = document.querySelector('#ramen-detail')
-    ramenMenu.addEventListener('click', (e)=> {
-        selectedRamenId = e.target.dataset.id
-        ramenDetail.append(detailImage)
-        displayDetailById(selectedRamenId)
-    })
+const renderRamen = (ramenObj) => {
+    const ramenMenu = document.querySelector('#ramen-menu');
+    let ramenImage = document.createElement('img');
+    ramenImage.src = ramenObj.image;
+    ramenMenu.append(ramenImage);
+    handleClick(ramenImage, ramenObj);
 };
 
-function displayDetailById (id) {
-    const selected = ramens.find(oneRamen => oneRamen.id === parseInt(id))
-    const image = document.querySelector('.detail-image')
-    const name = document.querySelector('.name')
-    const restaurant = document.querySelector('.restaurant')
-    restaurant.textContent = selected.restaurant
-    name.textContent = selected.name
-    image.src = selected.image
-    console.log(selected.name)
+const handleClick = (image, ramen) => {
+    image.addEventListener('click', () => {
+        const detailImage = document.querySelector('.detail-image')
+        const detailName = document.querySelector('.name')
+        const detailRestaurant = document.querySelector('.restaurant')
+        const detailRating = document.querySelector('#rating-display')
+        const detailComment = document.querySelector('#comment-display')
+        detailImage.src = ramen.image
+        detailName.textContent = ramen.name
+        detailRestaurant.textContent = ramen.restaurant
+        detailRating.textContent = ramen.rating
+        detailComment.textContent = ramen.comment
+    });
+};
+
+const handleForm = () => {
+    const submitForm = document.querySelector('#new-ramen');
+    submitForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        let newName = e.target['name'].value;
+        let newRes = e.target['restaurant'].value;
+        let newImg = e.target['image'].value;
+        let newRating = e.target['rating'].value;
+        let newComment = e.target['new-comment'].value;
+        const newRamenObj = {
+            name: newName,
+            restaurant: newRes,
+            image: newImg,
+            rating: newRating,
+            comment: newComment
+        };
+        fetch('http://localhost:3000/ramens', {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(newRamenObj)
+        })
+        .then((res) => res.json())
+        .then((data) => renderRamen(data))
+    });
 };
 
 
-
-imageClick();
+handleForm();
